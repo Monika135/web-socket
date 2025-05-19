542db1cb7fed24ca6f985302574044d5420275c0
Monika <85540663+Monika135@users.noreply.github.com>
2025-05-19 14:27:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,21 +9,68 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")  # Allow CORS for Socket.IO
 
 
+# count = 0
+
+
+# def send_count():
+#     global count
+#     while True:
+#         count += 1
+#         socketio.emit('count_message', {'count': count})
+#         eventlet.sleep(3)
+
+
+# # @socketio.on('message')
+# # def handle_message(msg):
+# #     print('Received message: ' + msg)
+# #     send(msg, broadcast=True)
+
+
+# @app.route('/')
+# def index():
+#     return "Welcome to Flask WebSocket Example!"
+
+
+# @socketio.on('connect')
+# def handle_connect():
+#     print('Client connected')
+#     socketio.start_background_task(send_count)
+
+
+# @socketio.on('disconnect')
+# def handle_disconnect():
+#     print('Client disconnected')
+
 count = 0
+counting = False  # Flag to control background task
 
 
 def send_count():
-    global count
-    while True:
+    global count, counting
+    counting = True
+    while counting:
         count += 1
         socketio.emit('count_message', {'count': count})
-        eventlet.sleep(3)
+        eventlet.sleep(4)
 
 
-# @socketio.on('message')
-# def handle_message(msg):
-#     print('Received message: ' + msg)
-#     send(msg, broadcast=True)
+def reset_count():
+    global count, counting
+    counting = False  # Stop the loop
+    count = 0
+    socketio.emit('reset_count', {'count': 0})
+
+
+@socketio.on('count_message')
+def start_counting(data=None):
+    global counting
+    if not counting:  # Prevent multiple background tasks
+        socketio.start_background_task(send_count)
+
+
+@socketio.on('reset_count')
+def stop_counting():
+    reset_count()
 
 
 @app.route('/')
@@ -34,7 +81,6 @@ def index():
 @socketio.on('connect')
 def handle_connect():
     print('Client connected')
-    socketio.start_background_task(send_count)
 
 
 @socketio.on('disconnect')
