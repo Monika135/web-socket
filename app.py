9eccf24d02d10e1ac8fb4e8feb40afb980145872
Monika <85540663+Monika135@users.noreply.github.com>
2025-05-20 23:35:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 import eventlet
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins
+CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
