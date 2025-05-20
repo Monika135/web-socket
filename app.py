7b07c23b9b114a5659a57d66528aba7ff58169c0
Monika <85540663+Monika135@users.noreply.github.com>
2025-05-20 23:37:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+#1
 # def send_count():
 #     global count
 #     while True:
